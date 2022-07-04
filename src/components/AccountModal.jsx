977e0fe45dff37bee773134ff9d7e045856734a0
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-07-04 19:07:36 +0200

    animating the profile pic image uploader popup

--- src/components/AccountModal.jsx
+++ src/components/AccountModal.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {
     Popover,
     PopoverTrigger,
@@ -10,8 +10,12 @@ import {
     PopoverCloseButton,
     Avatar,
     useDisclosure,
+    transition,
 
   } from '@chakra-ui/react'
+
+import { motion } from 'framer-motion'
+import { useAnimation } from 'framer-motion'
 
 
 
@@ -28,12 +32,29 @@ const AccountModal = ({isOpen, onClose}) => {
   const [edit, setEdit] = useState(false)
   const [editInput, setEditInput] = useState(user.displayName)
   const [isActive, setIsActive] = useState(false)
+  const animation = useAnimation()
+
+
+  useEffect(() => {
+    if(isActive){
+        animation.start({
+            x: 10,
+            y: -10
+        })
+    } else {
+        animation.start({
+            x: -10,
+            y: 10
+        })
+    }
+  }, [isActive])
 
   console.log(user);
 
   const editUserName = () => {
     setEdit(true)
   }
+  
 
   const handleUsernameEdit = () => {
     if(editInput !== ""){
@@ -83,10 +104,10 @@ const AccountModal = ({isOpen, onClose}) => {
                             <p className='text-blue-500'>Email : </p>
                             <h2>{user.email}</h2>
                         </div>
-                        <div className={isActive ? ' shadow-md bg-gray-50 absolute left-60 top-10 flex gap-4 rounded items-center py-2 px-3' : 'hidden'}>
+                        <motion.div animate={animation} className={isActive ? ' shadow-md bg-gray-50 absolute left-60 top-10 flex gap-4 rounded items-center py-2 px-3' : 'hidden'}>
                             {!user.photoURL ? <Avatar /> : <Avatar src={user.photoURL} />}
                             <input type="file" className='w-36 file:text-white file:rounded file:bg-blue-500 file:border-none file:py-1 file:cursor-pointer' />
-                        </div>
+                        </motion.div>
                     </div>
                 </PopoverBody>
                 <PopoverFooter border='0'>
