36aac2620f9f29449d5dc6ee8be6188e36016bb2
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-07-05 12:01:19 +0200

    sidebar functionalities done.

--- src/components/AccountModal.jsx
+++ src/components/AccountModal.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {
     Popover,
-    PopoverTrigger,
     PopoverContent,
     PopoverHeader,
     PopoverBody,
@@ -9,21 +8,20 @@ import {
     PopoverArrow,
     PopoverCloseButton,
     Avatar,
-    useDisclosure,
-    transition,
+
 
   } from '@chakra-ui/react'
 
 import { motion } from 'framer-motion'
 import { useAnimation } from 'framer-motion'
 
-
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faCheck } from '@fortawesome/free-solid-svg-icons'
-
+import { faEdit, faCheck, faClose } from '@fortawesome/free-solid-svg-icons'
 import { useAuthContext } from '../context/AuthContext'
 import { auth } from '../firebase'
+import { storage } from '../firebase'
+import {ref, uploadBytes, getDownloadURL} from "firebase/storage"
+
 
 
 const AccountModal = ({isOpen, onClose}) => {
@@ -31,8 +29,18 @@ const AccountModal = ({isOpen, onClose}) => {
   const user = useAuthContext()
   const [edit, setEdit] = useState(false)
   const [editInput, setEditInput] = useState(user.displayName)
+  const [image, setImage] = useState()
   const [isActive, setIsActive] = useState(false)
   const animation = useAnimation()
+  const [imgUrl, setImgUrl] = useState(user.photoURL)
+  const [isLoading, setIsLoading] = useState(false)
+
+
+  useEffect(() => {
+    auth.currentUser.updateProfile({
+        photoURL: imgUrl
+    })
+  }, [imgUrl])
 
 
   useEffect(() => {
@@ -47,9 +55,9 @@ const AccountModal = ({isOpen, onClose}) => {
             y: 10
         })
     }
+
   }, [isActive])
 
-  console.log(user);
 
   const editUserName = () => {
     setEdit(true)
@@ -69,6 +77,35 @@ const AccountModal = ({isOpen, onClose}) => {
     setIsActive(!isActive)
   }
 
+  const editProfilePic = (e) => {
+    if(e.target.files[0]){
+        setImage(e.target.files[0])
+    }
+  }
+
+  const handleImageSubmit = async () => {
+    const imageRef = ref(storage, "image")
+
+    
+    uploadBytes(imageRef, image).then(() => {
+        
+        getDownloadURL(imageRef).then(url => {
+
+            setImgUrl(url)
+        }).catch(error => {
+            console.log(error.message);
+        })
+        setIsLoading(false)
+    }).catch(error => {
+        console.log(error.message);
+    })
+
+    
+  }
+
+
+
+
   return (
 
 
@@ -83,7 +120,7 @@ const AccountModal = ({isOpen, onClose}) => {
                     <div className='flex flex-col items-center gap-4 py-6'>
 
                         <div className='flex gap-2 text-center'>
-                            {!user.photoURL ? <Avatar /> : <Avatar src={user.photoURL} />}
+                            <Avatar src={imgUrl} />
                             <button>
                                 <FontAwesomeIcon icon={faEdit} className='text-blue-500' onClick={toggleEditImgLabel} />
                             </button>
@@ -104,9 +141,21 @@ const AccountModal = ({isOpen, onClose}) => {
                             <p className='text-blue-500'>Email : </p>
                             <h2>{user.email}</h2>
                         </div>
-                        <motion.div animate={animation} className={isActive ? ' shadow-md bg-gray-50 absolute left-60 top-10 flex gap-4 rounded items-center py-2 px-3' : 'hidden'}>
-                            {!user.photoURL ? <Avatar /> : <Avatar src={user.photoURL} />}
-                            <input type="file" className='w-36 file:text-white file:rounded file:bg-blue-500 file:border-none file:py-1 file:cursor-pointer' />
+                        <motion.div animate={animation} className={isActive ? ' shadow-md bg-gray-50 absolute left-60 top-10 rounded py-2 px-3' : 'hidden'}>
+                            <div className='flex flex-col gap-6 relative'>
+
+                                <div className='flex items-center'>           
+                                    <h1>Edit Profile Picture</h1>
+                                    <FontAwesomeIcon icon={faClose} onClick={() => setIsActive(false)} className='absolute right-0 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </div>
+                                <div className='flex gap-4 items-center'>
+                                    <Avatar src={imgUrl}  />
+                                    <input type="file" onChange={(e) => editProfilePic(e)} className='w-36 file:text-white file:rounded file:bg-blue-500 file:border-none file:py-1 file:cursor-pointer' />
+                                    <button className='text-white bg-blue-500 font-bold px-3 py-1 rounded' onClick={handleImageSubmit}>{isLoading ? "Loading..." : "Save"} </button>
+                                </div>
+                            </div>
+                            
+                    
                         </motion.div>
                     </div>
                 </PopoverBody>
