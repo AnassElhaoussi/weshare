b61b101f45932126847578c0dd51cb2af0496669
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-07-10 16:45:10 +0200

    maping through the array of posts in cloud firstore db

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,7 @@ import { faEarth } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { db } from '../firebase'
 
+
 const Home = () => {
 
   const user = useAuthContext()
@@ -24,31 +25,43 @@ const Home = () => {
     })
 
   },[isClicked])
-
+  
+  const sharePost = async () => {
+    await db.collection('posts').add({
+      postText: postText
+    })
+  }
   
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
-      snapshot.docs.forEach(doc => {
-        console.log(doc.data());
+      db.collection('posts').onSnapshot(snapshot => {
+        setPosts(
+          snapshot.docs.map(doc => 
+            doc.data()
+          )
+        )
+
       })
-    })
 
-
+      console.log(posts);
   }, [])
+
+
+
+  
 
   
 
   return (
-    <div className='flex flex-col flex-shrink gap-6 w-2/3 relative'>
+    <div className='flex flex-col flex-shrink w-2/3 relative'>
         <button className='bg-gray-100 py-2 px-4 sm:w-1/2 text-gray-400 shadow-md rounded cursor-pointer flex-shrink-0'
         onClick={() => setIsClicked(!isClicked)}
         >
           {`What's on your mind, ${user.displayName}?`}
         </button>
-        
-        {
-          isClicked && (
-            <motion.div animate={animation} initial={{y: 20}} className='flex flex-col gap-6 md:w-1/2 w-full absolute top-14 bg-gray-100 p-4 rounded-md shadow-lg'>
+
+    {isClicked && (
+
+        <motion.div animate={animation} initial={{y: 20}} className='absolute top-20 flex flex-col gap-6 md:w-1/2 w-full bg-gray-100 p-4 rounded-md shadow-lg'>
                 <div className='flex gap-4 items-center'>
                   <Avatar src={user.photoURL} />
                   <div className='flex flex-col gap-1'> 
@@ -67,12 +80,19 @@ const Home = () => {
                    ></textarea>
                 </div>
                 <div className='flex justify-between text-white font-bold'>
-                  <button className='text-white bg-blue-500 py-1 px-3 rounded hover:scale-105 transition-all'>Share</button>
+                  <button className='text-white bg-blue-500 py-1 px-3 rounded hover:scale-105 transition-all' 
+                  onClick={sharePost}
+                  >Share</button>
                   <button className='text-white bg-blue-500 py-1 px-3 rounded hover:scale-105 transition-all' onClick={() => setIsClicked(false)}>Cancel</button>
                 </div>
-            </motion.div>
-          )
-        }
+        </motion.div>
+        
+    )}
+    <div>
+      {posts.map(post => {
+        <h1>{post.postText}</h1>
+      })}
+    </div>
     </div>
   )
 }
